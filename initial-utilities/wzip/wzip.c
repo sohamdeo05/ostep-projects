e5603109f7064fdec53afdc833b97cca97579c4b
Soham <135415990+sohamdeo05@users.noreply.github.com>
2025-08-28 23:54:49 +0530

    Completed wzip

--- initial-utilities/wzip/wzip.c
+++ initial-utilities/wzip/wzip.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//size_t fwrite(const void ptr[restrict .size * .nmemb],size_t size, size_t nmemb, FILE *restrict stream);
+
+
 int main(int argc, char* argv[]){
-    const char *word = argv[1];
-
-    int counter = 0;
-    char buffer[1024];
-
-    FILE *mem = fmemopen(buffer, sizeof(buffer), "w");
-    if (!mem) {
-        perror("fmemopen");
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
         return 1;
     }
 
-    for(int i = 0; *(word + i) != '\0'; i++){
-        if(word[i] == word[i+1]){
-            counter++;
+    int prev = -1;
+    int counter = 1;
+
+    for (int f = 1; f < argc; f++) {
+        FILE *fp = fopen(argv[f], "r");
+        if (fp == NULL) {
+            printf("wzip: cannot open file\n");
+            return 1;
         }
-        else{
-            fwrite(word[i], );
+        
+        int c;
+        while ((c = fgetc(fp)) != EOF) {
+            if (prev == -1) {
+                prev = c;
+                counter = 1;
+            }
+            else if (c == prev) {
+                counter++;
+            } 
+            else {
+                fwrite(&counter, sizeof(int), 1, stdout);
+                fwrite(&prev, sizeof(char), 1, stdout);
+                counter = 1;
+                prev = c;
+            }
         }
+
+        fclose(fp);
     }
-    return 0;
+    if (counter > 0) {
+        fwrite(&counter, sizeof(int), 1, stdout);
+        fwrite(&prev, sizeof(char), 1, stdout);
+    }
+
 }
