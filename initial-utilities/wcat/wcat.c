494c76211f81e1126044b1d5a14400618b6db511
Soham <135415990+sohamdeo05@users.noreply.github.com>
2025-08-28 16:42:08 +0530

    Completed wcat

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        printf("Usage: wcat <filename>\n");
+    }
+
+    const char *mode = "r";
+    char buffer[256];
+    FILE *fptr = fopen(argv[1], mode);
+
+    if(fptr == NULL){
+        printf("File not found.\n");
+    }
+
+    while(fgets(buffer, sizeof(buffer), fptr)){
+        printf("%s", buffer);
+    }
+    
+    fclose(fptr);
+    return 1;
+}
