3aa4a3754aca48c682e3d0132c17c7cfab51197c
Soham <135415990+sohamdeo05@users.noreply.github.com>
2025-08-28 17:05:23 +0530

    Modified code to pass [ https://github.com/remzi-arpacidusseau/ostep-projects/tree/master/initial-utilities/wcat/tests ]

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -2,21 +2,26 @@
 
 int main(int argc, char *argv[]){
     if(argc < 2){
-        printf("Usage: wcat <filename>\n");
+        // printf("Usage: wcat <filename>\n");
+        return 0;
     }
 
     const char *mode = "r";
     char buffer[256];
-    FILE *fptr = fopen(argv[1], mode);
 
-    if(fptr == NULL){
-        printf("File not found.\n");
-    }
+    for(int i = 1; i < argc; i++ ){
+        FILE *fptr = fopen(argv[i], mode);
 
-    while(fgets(buffer, sizeof(buffer), fptr)){
-        printf("%s", buffer);
+        if(fptr == NULL){
+            printf("wcat: cannot open file\n");
+            return 1; 
+        }
+
+        while(fgets(buffer, sizeof(buffer), fptr)){
+            printf("%s", buffer);
+        }
+        fclose(fptr);
     }
-    fclose(fptr);
-    printf("\n");
+    // printf("\n");
     return 0;
 }
